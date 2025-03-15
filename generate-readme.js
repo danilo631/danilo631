822cdf248ebed58386f5bdfce69ded24be2f0807
danilo631 <defreitasfernandesdanilo81@gmail.com>
2025-03-15 16:43:41 -0300

    Update generate-readme.js

--- generate-readme.js
+++ generate-readme.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 
 // Configurações
 const GITHUB_USERNAME = "danilo631"; // Substitua pelo seu nome de usuário do GitHub
-const GITHUB_TOKEN = process.env.API_TOKEN; // Novo nome do token
+const GITHUB_TOKEN = process.env.API_TOKEN; // Token do GitHub
 
 // Função para buscar repositórios do GitHub
 async function fetchRepositories() {
